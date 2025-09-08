8ce7c2139bde90bba76ce71bb09e93b2bfbe747f
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-08 15:03:58 -0300

    salvar

---
+++ 8setembro/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import InfoCliente from './components/InfoCliente'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <InfoCliente />
+      
+    
+    </>
+  )
+}
+
+export default App
